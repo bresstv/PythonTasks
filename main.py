b6447a416345872f1b2f10a73ef2eb0b92deccdd
Nero <draemond@MacBook-Pro-Nero.local>
2026-06-18 15:32:09 +0300

    update main.py file_1

---
+++ main.py
@@ -0,0 +1,31 @@
+# # 1
+# number_1 = int(input("enter the first number:\n"))
+# number_2 = int(input("enter the second number:\n"))
+# print(number_1 > number_2)
+
+# # 2
+# text = input("Hi world")
+# print(len(text) > 5)
+
+# # 3
+# world = input("enter the world:\n")
+# print(world[-1])
+
+# # 4
+# password = "p1r3u4"
+# confirm_password = input("enter the password:\n")
+# if password == confirm_password:
+#     print("spanked sovpadut")
+
+# # 5
+# text_1 = input("Задайте текст")
+# text_2 = input("Задайте текст")
+# print(text_1 == text_2)
+
+# 6 
+
+import colorama
+colorama.init(autoreset= True)
+RED = colorama.Fore.RED
+hello = f"{RED}здаров"
+print(hello)
